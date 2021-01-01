9793cabbcebbec5afd9ea822523fec02e7b71ea2
Marcelino Jannuzzi <jannuzzimarcelino1@gmail.com>
2021-01-01 17:05:21 -0300

    some improvements, need some refactor

--- DownloaderApp/index.js
+++ DownloaderApp/index.js
@@ -2,10 +2,9 @@ const { API_KEY } = require("./api");
 
 const { json } = require("./fakeData");
 
-
 const listPage = document.getElementById("list");
 const detailsPage = document.getElementById("details");
-// const video = document.getElementById("vp");
+const video = document.getElementById("vp");
 
 const btnDownload = document.getElementById("btnDownload");
 const tbody = document.getElementById("tc");
@@ -14,24 +13,28 @@ const inputBtn = document.getElementById("searchBtn");
 const backBtn = document.getElementById("btnVoltar");
 
 const downloadModal = document.getElementById("download-modal");
+const loadingModal = document.getElementById("loading-modal")
 
-const downloadVideoBtn = document.getElementById("DownloadTabBtn");
+const downloadVideoBtn = document.getElementById("downloadTabBtn");
+const cancelDownloadVideoBtn = document.getElementById("cancelDownloadVideoBtn");
 
+const gifCatDownloading = "https://media.giphy.com/media/LHZyixOnHwDDy/giphy.gif"
+const gifCatDownloadDone = "https://media.giphy.com/media/jQWUkD7a4AWfkraBJa/giphy.gif"
 
-
-
+const gif = document.getElementById('gif')
+const downloadLine = document.getElementById('downloading-line');
 
 const get = (keyword) => {
   let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=${API_KEY}&type=video&q=${keyword}`;
-  tbody.innerHTML = '';
+  tbody.innerHTML = "";
   fetch(url)
       .then(response => response.json())
       .then(json =>  showInfo(json))
-
+  // showInfo(json);
 };
 
 let showDetails = (info) => {
-  // video.src = setUrl(info.id.videoId);
+  video.src = setUrl(info.id.videoId);
   listPage.style.display = "none";
   detailsPage.style.display = "block";
   btnDownload.addEventListener("click", () => download(info));
@@ -46,8 +49,6 @@ let backToList = () => {
 let showInfo = (json) => {
   let items = json.items;
   items.map((item, index) => {
-    
-
     let th = document.createElement("tr");
 
     let id = document.createTextNode(index + 1);
@@ -103,27 +104,51 @@ let download = (info) => {
 };
 
 let downloadVideo = (info) => {
-  downloadModal.style.display = "none";
+  closeDownloadModal();
+  openLoadingModal()
   let data = {
     VideoId: info.id.videoId,
-    Title: info.snippet.title
-  }
-  
-  fetch('https://localhost:5001/api/Videos', {
-    method: 'POST',
+    Title: info.snippet.title,
+  };
+
+  fetch("https://localhost:5001/api/Videos", {
+    method: "POST",
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(data)
-  }).then(response => response.json())
-    .then(json => console.log(json))
-    
-}
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      closeLoadingModal()
+    });
+};
 
 let downloadAudio = () => {
   return null;
 };
 
+let closeDownloadModal = () => {
+  downloadModal.style.display = "none";
+};
+
+let openLoadingModal = () => {
+  downloadLine.innerHTML = "Downloading";
+  gif.src = gifCatDownloading;
+  loadingModal.style.display = 'flex';
+}
+let closeLoadingModal = () => {
+  downloadLine.innerHTML = "Done";
+
+  gif.src = gifCatDownloadDone;
+  setTimeout(() => loadingModal.style.display = 'none', 2000);
+
+  
+  
+}
+
 inputBtn.addEventListener("click", () => get(inputField.value));
 
+cancelDownloadVideoBtn.addEventListener("click", () => closeDownloadModal());
